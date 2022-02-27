f3dabdd05cfd54984428eeb4a1f03db1e300147e
ge43niy <ge43niy@tum.de>
2022-02-27 16:49:16 +0100

    Working tests

--- src/assets/loadFiles.js
+++ src/assets/loadFiles.js
@@ -10,73 +10,79 @@ const saveFS = () => {
 
 function initEditorGist() {
   const gistId = location.hash.substring(1);
-  if (location.hash && gistId.length === 32) {
-    console.log("Found task");
+  if (!location.hash || gistId.length !== 32) {
+    throw new Error("Could not retrireve tasks");
+  }
+  console.log("Found task");
 
-    if (localStorage.getItem("fs" + gistId)) {
-      fs = JSON.parse(localStorage.getItem("fs" + gistId));
-      window.tests = JSON.parse(localStorage.getItem("test" + gistId));
-      addToTabs(getFile("main.tex"), "main.tex");
-      openFile("main.tex");
-    } else {
-      fs = {
-        label: "root",
-        type: "Directory",
-        children: [
-          {
-            label: "assets",
-            type: "Directory",
-            children: [
-              {
-                label: "images",
-                type: "Directory",
-                children: [
-                  {
-                    label: "README.md",
-                    type: "File",
-                    contents: `# Readme\n\nIn the assets folder you can 'upload' personal files.
+  if (localStorage.getItem("fs" + gistId)) {
+    fs = JSON.parse(localStorage.getItem("fs" + gistId));
+    window.tests = JSON.parse(localStorage.getItem("test" + gistId));
+    addToTabs(getFile("main.tex"), "main.tex");
+    openFile("main.tex");
+
+    renderTests(window.tests);
+    $files.innerHTML = getTreeHTML(fs);
+    updateTree();
+  } else {
+    fs = {
+      label: "root",
+      type: "Directory",
+      children: [
+        {
+          label: "assets",
+          type: "Directory",
+          children: [
+            {
+              label: "images",
+              type: "Directory",
+              children: [
+                {
+                  label: "README.md",
+                  type: "File",
+                  contents: `# Readme\n\nIn the assets folder you can 'upload' personal files.
 None of them leave your device of course.
 Upload images or other TeX files and work with them inside of your projects.
 
 ## How to upload?
 
 Just drag and drop the file(s), you will have visual feedback.`,
-                  },
-                ],
-              },
-            ],
-          },
-        ],
-      };
-      fetch("https://api.github.com/gists/" + gistId, {
-        method: "GET",
-      })
-        .then((result) => result.json())
-        .then((result) => {
-          for (const fileName in result.files) {
-            if (Object.hasOwnProperty.call(result.files, fileName)) {
-              const file = result.files[fileName];
-              switch (fileName) {
-                case "solution.tex":
-                  console.log("Hide solutions");
-                  break;
-                case "tests.json":
-                  window.tests = JSON.parse(file.content);
-                  renderTests(tests);
-                  localStorage.setItem("test" + gistId, file.content);
-                  break;
-                default:
-                  createFile(file.filename, file.content, file.type, "");
-                  break;
-              }
+                },
+              ],
+            },
+          ],
+        },
+      ],
+    };
+    fetch("https://api.github.com/gists/" + gistId, {
+      method: "GET",
+    })
+      .then((result) => result.json())
+      .then((result) => {
+        for (const fileName in result.files) {
+          if (Object.hasOwnProperty.call(result.files, fileName)) {
+            const file = result.files[fileName];
+            switch (fileName) {
+              case "solution.tex":
+                console.log("Hide solutions");
+                break;
+              case "tests.json":
+                window.tests = JSON.parse(file.content);
+                localStorage.setItem("test" + gistId, file.content);
+                break;
+              default:
+                createFile(file.filename, file.content, file.type, "");
+                break;
             }
           }
-          saveFS();
-        })
-        .catch((e) => console.error(e));
-    }
+        }
+        saveFS();
+      })
+      .catch((e) => console.error(e))
+      .finally(() => {
+        renderTests(window.tests);
+        $files.innerHTML = getTreeHTML(fs);
+        updateTree();
+      });
   }
-  renderTests(tests);
-  $files.innerHTML = getTreeHTML(fs);
-  updateTree();
 }
--- src/assets/tests.js
+++ src/assets/tests.js
@@ -8,7 +8,7 @@ function renderTests(tests = []) {
     $tasks.innerHTML += `
         <div class="accordion-item">
             <h2 class="accordion-header btn-group btn-block btn-test-run" id="test-run-${i}">
-                <button class="btn btn-primary flex-grow-0">
+                <button onclick="runTest(${i})" class="btn btn-primary flex-grow-0" id="btn-test-run-${i}">
                     <i class="codicon codicon-debug-alt"></i>
                 </button>
                 <button class="btn btn-secondary flex-grow-1 text-left" type="button"
@@ -29,4 +29,30 @@ function renderTests(tests = []) {
   }
 }
 
-document.getElementsByClassName("btn-test-run");
+/**
+ * Tests the given test and gives ui feedback.
+ * @param {number} testNr the test to evaluate
+ */
+function runTest(testNr = 0) {
+  const $test = document.getElementById("btn-test-run-" + testNr);
+  try {
+    if (!tests) throw new Error("GLOBAL Test object does not exist");
+    if (testNr < 0 || testNr >= tests.length)
+      throw new Error("Test does not exsist");
+
+    const test = new RegExp(tests[testNr].test);
+    const testFile = getFile(tests[testNr].file);
+    console.log(testFile.contents, test);
+    if (!test.test(testFile.contents)) {
+      throw new Error("Test failed.");
+    }
+
+    $test.classList.remove("btn-primary");
+    $test.classList.remove("btn-danger");
+    $test.classList.add("btn-success");
+  } catch (error) {
+    $test.classList.remove("btn-primary");
+    $test.classList.remove("btn-success");
+    $test.classList.add("btn-danger");
+  }
+}
